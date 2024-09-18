59688f9a5ea56731709d71dcc1b2d0bdba12781a
Creatlydev <creatlydev@gmail.com>
2024-09-17 22:52:21 -0500

    feat: Add search bar with styles and transitions, and improve header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,35 @@
+import { useState, useEffect } from 'react'
+
 import './Header.css'
-import { MagniferLinear } from './Icons'
+import { CloseCircleOutline, MagniferLinear } from './Icons'
 import Navbar from './Navbar'
 
 export default function Header () {
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
+  const [navUp, setNavUp] = useState(false)
+
+  useEffect(() => {
+    let lastScrollTop = 0
+    const toogleHeaderVisbility = () => {
+      const currentScrollTop = window.scrollY || document.documentElement.scrollTop
+
+      if (currentScrollTop > lastScrollTop) {
+        currentScrollTop > 72 && setNavUp(true)
+      } else {
+        setNavUp(false)
+      }
+      lastScrollTop = currentScrollTop
+    }
+
+    window.addEventListener('scroll', toogleHeaderVisbility)
+
+    return () => {
+      window.removeEventListener('scroll', toogleHeaderVisbility)
+    }
+  }, [])
+
   return (
-    <header className='HeaderMain'>
+    <header className={`HeaderMain ${navUp ? 'Nav-up' : 'Nav-down'}`}>
       <div className='container'>
         <div className='Logo'>
           <img src='/src/assets/logo.svg' alt='Logo de the movie db' />
@@ -12,9 +37,20 @@ export default function Header () {
 
         <Navbar />
 
-        <button>
-          <MagniferLinear />
+        <button onClick={() => setIsSearchVisible(!isSearchVisible)}>
+          {
+            isSearchVisible
+              ? <CloseCircleOutline />
+              : <MagniferLinear />
+          }
         </button>
+      </div>
+
+      <div className={`SearchBar ${isSearchVisible ? 'is-visible' : 'is-hidden'}`}>
+        <form className='SearchBar-form container'>
+          <MagniferLinear style={{ width: '20px', height: '16px' }} />
+          <input className='field' type='search' placeholder='Busca una pelicula, serie o persona' />
+        </form>
       </div>
     </header>
   )
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -6,6 +6,12 @@ export function MagniferLinear (props) {
 
 export function ArrowDownLinear (props) {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' {...props}><path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.5' d='m19 9l-7 6l-7-6' /></svg>
+    <svg xmlns='http://www.w3.org/2000/svg' width='24px' height='24px' viewBox='0 0 24 24' {...props}><path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='1.5' d='m19 9l-7 6l-7-6' /></svg>
+  )
+}
+
+export function CloseCircleOutline (props) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24px' height='24px' viewBox='0 0 24 24' {...props}><path fill='currentColor' d='M10.03 8.97a.75.75 0 0 0-1.06 1.06L10.94 12l-1.97 1.97a.75.75 0 1 0 1.06 1.06L12 13.06l1.97 1.97a.75.75 0 0 0 1.06-1.06L13.06 12l1.97-1.97a.75.75 0 1 0-1.06-1.06L12 10.94z' /><path fill='currentColor' fillRule='evenodd' d='M12 1.25C6.063 1.25 1.25 6.063 1.25 12S6.063 22.75 12 22.75S22.75 17.937 22.75 12S17.937 1.25 12 1.25M2.75 12a9.25 9.25 0 1 1 18.5 0a9.25 9.25 0 0 1-18.5 0' clipRule='evenodd' /></svg>
   )
 }
